7a9e32c91341022a2c1e51da1b5d4f04f0b9e094
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2023-02-14 21:10:49 +0900

    [back] 기본 프로젝트 세팅

---
+++ backend/src/main/java/ss/dapalza/DapalzaApplication.java
@@ -0,0 +1,13 @@
+package ss.dapalza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapalzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapalzaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ss/dapalza/DapalzaApplicationTests.java
@@ -0,0 +1,13 @@
+package ss.dapalza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DapalzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
