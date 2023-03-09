3247ceec80a972e8393984ff3beb9f0c05bce6c7
Yunjeong Choi / 최윤정 <92416563+Layton0-0@users.noreply.github.com>
2023-03-09 20:29:28 +0900

    [feat] 회원가입 백엔드 로컬 데이터 저장

--- backend/src/main/java/ss/dapalza/register/RegisterController.java
+++ backend/src/main/java/ss/dapalza/register/RegisterController.java
@@ -1,11 +1,37 @@
 package ss.dapalza.register;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ss.dapalza.dto.req.RegisterRequest;
+import ss.dapalza.dto.res.ErrorResponse;
+import ss.dapalza.dto.res.RegisterResponse;
+
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/register")
 public class RegisterController {
 
     private final RegisterService service;
+
+    @PostMapping()
+    public ResponseEntity<JSONObject> register(@RequestBody RegisterRequest req) {
+        JSONObject body = new JSONObject();
+        RegisterResponse res = service.registerCustomer(req);
+
+        if(res != null) {
+            body.put("res", res);
+            return new ResponseEntity<>(body, HttpStatus.OK);
+        } else {
+            JSONObject error = new JSONObject();
+            error.put("error", ErrorResponse.NOT_FOUND);
+            return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
